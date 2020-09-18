762a54ec56a1867e6d62cbb65fd088d64488d8b9
tinnakorn-M <tinnakorn21697@gmail.com>
2020-09-18 11:16:07 +0700

    Edit heatmap

--- src/app/heatmap/heatmap.component.ts
+++ src/app/heatmap/heatmap.component.ts
@@ -14,17 +14,7 @@ export class HeatmapComponent implements OnInit {
   // 103.2569
   lat = 16.240652812500006;
   lng = 103.25703583333335;
-  zoom = 18;
-  latitude: any;
-  longitude: any;
-  location: any;
-  
-  alldata;
-  lati;
-  longi;
-  frequency;
-  rssi;
-  snr;
+  zoom = 20;
 
   public map: google.maps.Map;
   public heatmap: google.maps.visualization.HeatmapLayer; 
@@ -47,17 +37,17 @@ export class HeatmapComponent implements OnInit {
     await this.http.get<[]>('http://202.28.34.197/LoRaTracker/location')
     .subscribe(data => {
       for (let index = 1; index < data.length; index++) {
-        this.lati = data[index]['latitude'];
-        this.longi = data[index]['longitude'];
-        this.frequency = data[index]['frequency'];
-        this.rssi = data[index]['rssi'];
-        this.snr = data[index]['snr'];
+        let lati = data[index]['latitude'];
+        let longi = data[index]['longitude'];
+        let frequency = data[index]['frequency'];
+        let rssi = data[index]['rssi'];
+        let snr = data[index]['snr'];
 
-        console.log(this.lati);
-        console.log(this.longi);
-        console.log(this.frequency);
-        console.log(this.rssi);
-        console.log(this.snr);
+        console.log(lati);
+        console.log(longi);
+        console.log(frequency);
+        console.log(rssi);
+        console.log(snr);
       }
     },error => {
       console.log(error);
@@ -84,78 +74,178 @@ export class HeatmapComponent implements OnInit {
     console.log(Date_Time);
 
     let info;
-    let locals = {latitude : mean_latitude, longitude : mean_longitude, rssi : mean_rssi, snr : mean_snr,
+    let locals = {latitude : mean_latitude.toFixed(6), longitude : mean_longitude.toFixed(6), rssi : mean_rssi, snr : mean_snr,
                   frequency : mean_frequency, time : Date_Time};
     localStorage.setItem(info, JSON.stringify(locals));
     let item = JSON.parse(localStorage.getItem(info));
     console.log(item);
   }
 
-  async onLoadHeatmap(LatLng){
+  async onLoadHeatmap(){
     var gradient = [
       'rgba(0, 0, 0, 0)', // white
       'rgba(0, 255, 0, 255)', // green
       'rgba(255, 255, 0, 255)',  // yellow
       'rgba(255,0,0,1)', 
     ]
-    let response = await this.http.get<[]>('http://202.28.34.197/LoRaTracker/location').toPromise();
+    let response = await this.http.get<[]>('http://202.28.34.197/LoRaTracker/SmartLocation/'+JSON.stringify(20)).toPromise();
+    // console.log(response);
 
-      for (let index = 40; index < 50; index++) {
-        let lati = response[index]['latitude'];
-        let longi = response[index]['longitude'];
-        let frequency = response[index]['frequency'];
-        let rssi = response[index]['rssi'];
-        let snr = response[index]['snr'];
+    let lati = Array();
+    let longi = Array();
+    let frequency = Array();
+    let snr = Array();
+    let rssi = Array();
+    let LatLng = Array();
 
-        // console.log(this.lati);
-        // console.log(this.longi);
-        // console.log(this.frequency);
-        // console.log(this.rssi);
-        // console.log(this.snr);
-        if(Math.abs(rssi) > 90){
-          this.rssi = 30;
-        }else if(Math.abs(rssi) > 800){
-          this.rssi = 40;
-        }else if(Math.abs(rssi) > 70){
-          this.rssi = 50;
-        }else if(Math.abs(rssi) > 60){
-          this.rssi = 60;
-        }else if(Math.abs(rssi) > 50){
-          this.rssi = 70;
-        }else if(Math.abs(rssi) > 40){
-          this.rssi = 10;
-        }else if(Math.abs(rssi) > 30){
-          this.rssi = 90;
-        }else if(Math.abs(rssi) > 20){
-          this.rssi = 100;
-        }else if(Math.abs(rssi) > 10){
-          this.rssi = 110;
-        }else{
-          this.rssi = 120;
-        }
-        console.log('rssi = ',rssi);
-        LatLng = [
-          {location: new google.maps.LatLng(lati, longi),weight:Math.abs(rssi)}
-        ];
-        // console.log(LatLng);
-        this.heatmap = new google.maps.visualization.HeatmapLayer({
-          map: this.map,
-          data: LatLng
-        });
-        // console.log(this.heatmap);
-        this.heatmap.set('gradient', this.heatmap.get('gradient') ? null : gradient);
-        this.heatmap.set('radius', this.heatmap.get('radius') ? null : 20);
-        this.heatmap.set('opacity', this.heatmap.get('opacity') ? null : 0.9);
-        this.heatmap.set('dissipating', this.heatmap.get('dissipating') ? null : true);
-        this.heatmap.set('maxIntensity', this.heatmap.get('maxIntensity') ? null : 0);
-      }
-      console.log('success');
     
+    for (let index = 0; index < response.length; index++) {
+      lati.push(response[index]['latitude']);
+      longi.push(response[index]['longitude']);
+      frequency.push(response[index]['frequency']);
+      rssi.push(response[index]['rssi']);
+      snr.push(response[index]['snr']);
+
+      // console.log(lati);
+      // console.log(longi);
+      // console.log(frequency);
+      // console.log(rssi);
+      // console.log(snr);
+      // if(Math.abs(rssi) > 90){
+      //   rssi = 30;
+      // }else if(Math.abs(rssi) > 800){
+      //   rssi = 40;
+      // }else if(Math.abs(rssi) > 70){
+      //   rssi = 50;
+      // }else if(Math.abs(rssi) > 60){
+      //   rssi = 60;
+      // }else if(Math.abs(rssi) > 50){
+      //   rssi = 70;
+      // }else if(Math.abs(rssi) > 40){
+      //   rssi = 10;
+      // }else if(Math.abs(rssi) > 30){
+      //   rssi = 90;
+      // }else if(Math.abs(rssi) > 20){
+      //   rssi = 100;
+      // }else if(Math.abs(rssi) > 10){
+      //   rssi = 110;
+      // }else{
+      //   rssi = 120;
+      // }
+      // console.log('rssi = ',rssi);
+      // LatLng = [
+      //   // {location: new google.maps.LatLng(lati, longi),weight:Math.abs(rssi)}
+      //   {location: new google.maps.LatLng(16.246261, 103.252502),weight:76}, // Fac   1 -76
+      //   {location: new google.maps.LatLng(16.246202, 103.252502),weight:67}, // Road  2 -85
+      //   {location: new google.maps.LatLng(16.246030, 103.252380),weight:69}, // Road  3 -83
+      //   {location: new google.maps.LatLng(16.245822, 103.252243),weight:65}, // Road  4 -87
+      //   {location: new google.maps.LatLng(16.245708, 103.252334),weight:64}, // Road  5 -88
+      //   {location: new google.maps.LatLng(16.245536, 103.252510),weight:63}, // Silk  6 -89
+      //   {location: new google.maps.LatLng(16.245416, 103.252685),weight:55}, // Road  7 -97 
+      //   {location: new google.maps.LatLng(16.245252, 103.252937),weight:73}, // Road  8 -79
+      //   {location: new google.maps.LatLng(16.245145, 103.253074),weight:55}, // Road  9 -97
+      //   {location: new google.maps.LatLng(16.245019, 103.253219),weight:58}, // Road  10 -94 
+      //   {location: new google.maps.LatLng(16.244956, 103.253379),weight:65}, // Road  11 -81
+      //   {location: new google.maps.LatLng(16.245132, 103.253433),weight:63}, // Road  12 -89  
+      //   {location: new google.maps.LatLng(16.245328, 103.253486),weight:78}, // Road  13 -74
+      //   {location: new google.maps.LatLng(16.245596, 103.253486),weight:77}, // Road  14 -77
+      //   {location: new google.maps.LatLng(16.245859, 103.253433),weight:79}, // Road  15 -73
+      //   {location: new google.maps.LatLng(16.246112, 103.253395),weight:82}, // Road  16 -70
+      //   {location: new google.maps.LatLng(16.246335, 103.253356),weight:67}, // Road  17 -85
+      //   {location: new google.maps.LatLng(16.246583, 103.253242),weight:73}, // Road  18 -79
+      //   {location: new google.maps.LatLng(16.246805, 103.253067),weight:72}, // Road  19 -80
+      //   {location: new google.maps.LatLng(16.246660, 103.252876),weight:76}, // Road  20 -76
+      //   {location: new google.maps.LatLng(16.246471, 103.252754),weight:81}, // Road  21 -71
+      //   {location: new google.maps.LatLng(16.246397, 103.252677),weight:64}, // Road  22 -88
+      //   {location: new google.maps.LatLng(16.246467, 103.252540),weight:71}, // Road  23 -81
+      // ];
+      // // console.log(LatLng);
+      // this.heatmap = new google.maps.visualization.HeatmapLayer({
+      //   map: this.map,
+      //   data: LatLng
+      // });
+      // // console.log(this.heatmap);
+      // this.heatmap.set('gradient', this.heatmap.get('gradient') ? null : gradient);
+      // this.heatmap.set('radius', this.heatmap.get('radius') ? null : 20);
+      // this.heatmap.set('opacity', this.heatmap.get('opacity') ? null : 0.9);
+      // this.heatmap.set('dissipating', this.heatmap.get('dissipating') ? null : true);
+      // this.heatmap.set('maxIntensity', this.heatmap.get('maxIntensity') ? null : 0);
+    }
+    for (let index = 0; index < lati.length; index++) {
+      const element = lati[index];
+      const element1 = longi[index];
+      const element2 = rssi[index];
+
+      // console.log(Math.abs(index));
+      LatLng = [
+        {location: new google.maps.LatLng(lati[index-10], longi[index-15]),weight:Math.abs(70)}, //มันเอาชุดข้อมูลตัวสุดท้ายไปใช้
+        {location: new google.maps.LatLng(lati[index], longi[index]),weight:Math.abs(rssi[index])}
+      ];
+      
+    }
+    
+    this.heatmap = new google.maps.visualization.HeatmapLayer({
+      map: this.map,
+      data: LatLng
+    });
+    console.log(LatLng);
+    console.log(this.heatmap);
+    this.heatmap.set('gradient', this.heatmap.get('gradient') ? null : gradient);
+    this.heatmap.set('radius', this.heatmap.get('radius') ? null : 20);
+    this.heatmap.set('opacity', this.heatmap.get('opacity') ? null : 0.9);
+    this.heatmap.set('dissipating', this.heatmap.get('dissipating') ? null : true);
+    this.heatmap.set('maxIntensity', this.heatmap.get('maxIntensity') ? null : 0);
+    console.log('success');
   }
 
   async onMapLoad(mapInstance:  google.maps.Map,LatLng) {
     this.map = mapInstance;
     this.map.setCenter(new google.maps.LatLng(16.240584, 103.2569));
+
+
+      LatLng = [
+      {location: new google.maps.LatLng(16.246261, 103.252502),weight:76}, // Fac   1 -76
+      {location: new google.maps.LatLng(16.246202, 103.252502),weight:67}, // Road  2 -85
+      {location: new google.maps.LatLng(16.246030, 103.252380),weight:69}, // Road  3 -83
+      {location: new google.maps.LatLng(16.245822, 103.252243),weight:65}, // Road  4 -87
+      {location: new google.maps.LatLng(16.245708, 103.252334),weight:64}, // Road  5 -88
+      {location: new google.maps.LatLng(16.245536, 103.252510),weight:63}, // Silk  6 -89
+      {location: new google.maps.LatLng(16.245416, 103.252685),weight:55}, // Road  7 -97 
+      {location: new google.maps.LatLng(16.245252, 103.252937),weight:73}, // Road  8 -79
+      {location: new google.maps.LatLng(16.245145, 103.253074),weight:55}, // Road  9 -97
+      {location: new google.maps.LatLng(16.245019, 103.253219),weight:58}, // Road  10 -94 
+      {location: new google.maps.LatLng(16.244956, 103.253379),weight:65}, // Road  11 -81
+      {location: new google.maps.LatLng(16.245132, 103.253433),weight:63}, // Road  12 -89  
+      {location: new google.maps.LatLng(16.245328, 103.253486),weight:78}, // Road  13 -74
+      {location: new google.maps.LatLng(16.245596, 103.253486),weight:77}, // Road  14 -77
+      {location: new google.maps.LatLng(16.245859, 103.253433),weight:79}, // Road  15 -73
+      {location: new google.maps.LatLng(16.246112, 103.253395),weight:82}, // Road  16 -70
+      {location: new google.maps.LatLng(16.246335, 103.253356),weight:67}, // Road  17 -85
+      {location: new google.maps.LatLng(16.246583, 103.253242),weight:73}, // Road  18 -79
+      {location: new google.maps.LatLng(16.246805, 103.253067),weight:72}, // Road  19 -80
+      {location: new google.maps.LatLng(16.246660, 103.252876),weight:76}, // Road  20 -76
+      {location: new google.maps.LatLng(16.246471, 103.252754),weight:81}, // Road  21 -71
+      {location: new google.maps.LatLng(16.246397, 103.252677),weight:64}, // Road  22 -88
+      {location: new google.maps.LatLng(16.246467, 103.252540),weight:71}, // Road  23 -81
+    ];
+    this.heatmap = new google.maps.visualization.HeatmapLayer({         
+        map: this.map,
+        data: LatLng
+    });
+    //https://developers-dot-devsite-v2-prod.appspot.com/maps/documentation/javascript/examples/layer-heatmap
+    var gradient = [
+      'rgba(0, 0, 0, 0)', // white
+      'rgba(0, 255, 0, 255)', // green
+      'rgba(255, 255, 0, 255)',  // yellow
+      'rgba(255,0,0,1)', 
+    ]
+    console.log('LatLng ',LatLng);
+    console.log('LatLng ',this.heatmap);
+    this.heatmap.set('gradient', this.heatmap.get('gradient') ? null : gradient);
+    this.heatmap.set('radius', this.heatmap.get('radius') ? null : 50);
+    this.heatmap.set('opacity', this.heatmap.get('opacity') ? null : 0.9);
+    this.heatmap.set('dissipating', this.heatmap.get('dissipating') ? null : true);
+    this.heatmap.set('maxIntensity', this.heatmap.get('maxIntensity') ? null : 0);
   }
 
 }
