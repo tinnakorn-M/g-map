e1aab55a7e1cda4f9995fa6fd4efb290fbc55445
tinnakorn-M <tinnakorn21697@gmail.com>
2020-10-23 13:53:30 +0700

    edit session

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -49,24 +49,25 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {} 
 
   async ngOnInit() {
-    var item = JSON.parse(localStorage.getItem('info'));
+    var item = JSON.parse(sessionStorage.getItem('info'));
     // console.log(item);
+    if(item != null){
+      this.mean_latitude = item.latitude;
+      this.mean_longitude = item.longitude;
+      this.mean_rssi = item.rssi;
+      this.mean_snr = item.snr;
+      this.mean_frequency = item.frequency;
+      this.mean_time = item.time;
+      this.distanceGW1 = item.distanceGW1;
+      this.distanceGW2 = item.distanceGW2;
+      this.fromgateway = item.fromgateway;
 
-    this.mean_latitude = item.latitude;
-    this.mean_longitude = item.longitude;
-    this.mean_rssi = item.rssi;
-    this.mean_snr = item.snr;
-    this.mean_frequency = item.frequency;
-    this.mean_time = item.time;
-    this.distanceGW1 = item.distanceGW1;
-    this.distanceGW2 = item.distanceGW2;
-    this.fromgateway = item.fromgateway;
-
-    this.min_rssi = item.min_rssi;
-    this.max_rssi = item.max_rssi;
-    this.min_snr = item.min_snr;
-    this.max_snr = item.max_snr;
-
+      this.min_rssi = item.min_rssi;
+      this.max_rssi = item.max_rssi;
+      this.min_snr = item.min_snr;
+      this.max_snr = item.max_snr;
+    }
+    
   }
 
   closeNav() {
